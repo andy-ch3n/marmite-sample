da8c3f30e00c8e563d8fe17c53d8663ace2ff874
Andy Chen <adch3n@gmail.com>
2022-08-09 15:50:23 -0700

    initial

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  env: {
+    CONTENTFUL_SPACE_ID: 'l43kl85sfapw',
+    CONTENTFUL_ACCESS_KEY: '-cUAYLOZ_6qC8BJjRtXZsF7i5V3lqN7jng8P6cuVrSg',
+  },
+  images: {
+    domains: ['images.ctfassets.net'],
+  },
+}
